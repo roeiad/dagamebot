f22e550095517afff91c7606fffcabca20cbb5c6
roeiad <roeiadlersh@gmail.com>
2020-07-07 14:39:39 +0300

    ss

--- liquid.js
+++ liquid.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 const d3 = require("d3-random")
-const liq = "./faces"
+const liq = "C:/Users/USER/WebstormProjects/dagamebot/faces"
 
 class liquid {
     static getFace() {
