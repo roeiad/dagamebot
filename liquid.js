4e7dac1539500695e7587dfbe1704b2375bf8d98
roeiad <roeiadlersh@gmail.com>
2020-07-07 15:03:41 +0300

    ss

--- liquid.js
+++ liquid.js
@@ -1,13 +1,14 @@
 const fs = require("fs")
 const d3 = require("d3-random")
 const liq="./faces"
-const path = "C:/Users/USER/WebstormProjects/dagamebot/faces"
+const path = "C:/Users/USER/WebstormProjects/dagamebot/faces/"
 
 class liquid {
     static getFace() {
         let files = fs.readdirSync(liq);
         let random = d3.randomUniform(0, files.length)
-        return files;
+        let face= files[random];
+        return path+face
 
     }
 }
