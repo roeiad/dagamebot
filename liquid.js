542ad4974bd2e8fa028f2884601608bd9ec0c400
roeiad <roeiadlersh@gmail.com>
2020-07-07 15:21:02 +0300

    ss

--- liquid.js
+++ liquid.js
@@ -4,16 +4,15 @@ const liq = "./faces"
 const path = "C:/Users/USER/WebstormProjects/dagamebot/faces/"
 
 class liquid {
-    static files() {
-        return fs.readdirSync(liq);
+    static getRandomArbitrary( max) {
+        return Math.random() * (max - 1) + 1;
     }
     static getFace() {
-        let files = this.files();
+        let files =  fs.readdirSync(liq);
         let amount=files.length
-        // let random = d3.randomUniform(0, amount)
-        // let face = files[random];
-        // return path + face
-return amount
+        let random = this.getRandomArbitrary(amount)
+        let face = files[random];
+        return path + face
     }
 }
 
