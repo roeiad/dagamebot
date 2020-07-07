9f899790b2d3006b7b8a02147095e1e5d241811d
roeiad <roeiadlersh@gmail.com>
2020-07-07 15:14:22 +0300

    ss

--- liquid.js
+++ liquid.js
@@ -9,10 +9,11 @@ class liquid {
     }
     static getFace() {
         let files = this.files();
-        let random = d3.randomUniform(0, files.length)
-        let face = files[random];
-        return path + face
-
+        let amount=files.length
+        // let random = d3.randomUniform(0, amount)
+        // let face = files[random];
+        // return path + face
+return amount
     }
 }
 
