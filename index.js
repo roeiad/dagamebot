8ce38246ffd79b329a5cc1b04a85620a955f3ea2
roeiad <roeiadlersh@gmail.com>
2020-08-16 13:49:03 +0300

    ss

--- index.js
+++ index.js
@@ -106,26 +106,27 @@ client.on("message", async message => {
         await message.channel.send("https://www.youtube.com/playlist?list=PLUurKioYqqsLjkDGjf8vNLgG6ZQpIgyGw");
     }
     if (command === "info") {
-        let twitFoll= info.twitterfolowers()
         const twitter = new Discord.MessageEmbed()
             .setTitle("twitter")
+            .setURL("https://twitter.com/DAGamesOfficial")
             .setColor(0x00ACEE)
             .setThumbnail("https://pbs.twimg.com/profile_images/1269240624504467456/uIigyOP1_200x200.jpg")
-            .addFields("followers",twitFoll)
             .setDescription("https://twitter.com/DAGamesOfficial")
         const youtube = new Discord.MessageEmbed()
             .setTitle("youtube")
+            .setURL("https://www.youtube.com/user/DiamondArmadaOnline")
             .setColor(0xFF0000)
             .setDescription(text.youtube)
             .setThumbnail("https://yt3.ggpht.com/a/AATXAJzb7yosXzm6PLx21qi2ZIlKy-xLKRO-bcomZWUVrA=s100-c-k-c0xffffffff-no-rj-mo")
             .setFooter(" ", "https://www.freepnglogos.com/uploads/youtube-logo-red-hd-13.png")
 
-        const embed3 = new Discord.MessageEmbed()
+        const twitch = new Discord.MessageEmbed()
             .setTitle("twitch")
+            .setURL("https://www.twitch.tv/dagamesofficial")
             .setColor(0x6441a5)
             .setDescription("https://www.twitch.tv/dagamesofficial")
-            .setFooter(" ", "https://www.freepnglogos.com/uploads/twitch-app-logo-png-3.png")
-        return [await message.channel.send({embed: embed1}), await message.channel.send({embed: embed2}), await message.channel.send({embed: embed3})]
+            .setThumbnail("https://images-ext-2.discordapp.net/external/6vWrXeRdB1RKYXLdgQ9el75ZAMcrODldV_qthRp6ssc/https/static-cdn.jtvnw.net/jtv_user_pictures/9fd13095-e26d-48e8-9332-f403a688a69e-profile_image-300x300.png")
+        return [await message.channel.send({embed: youtube}), await message.channel.send({embed: twitter}), await message.channel.send({embed: twitch})]
     }
     if (command === "help") {
         const embed = new Discord.MessageEmbed()
