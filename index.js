d49b06baeb58e9455d79c9625eb773d03f01d950
roeiad <roeiadlersh@gmail.com>
2020-08-16 12:25:53 +0300

    ss

--- index.js
+++ index.js
@@ -105,12 +105,12 @@ client.on("message", async message => {
         await message.channel.send("https://www.youtube.com/playlist?list=PLUurKioYqqsLjkDGjf8vNLgG6ZQpIgyGw");
     }
     if (command === "info") {
-        const embed1 = new Discord.RichEmbed()
+        const embed1 = new Discord.MessageEmbed()
             .setTitle("twitter")
             .setColor(0x00ACEE)
             .setImage("https://help.twitter.com/content/dam/help-twitter/brand/logo.png")
             .setDescription("https://twitter.com/DAGamesOfficial")
-      return await message.channel.send({embed1})
+      return await message.channel.send({embed:embed1})
         // const embed2 = new Discord.MessageEmbed()
         //     .setTitle("youtube")
         //     .setColor(0xFF0000)
