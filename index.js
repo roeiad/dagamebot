6dceae2ba6eff1284cb4f47e9e8e85bb8f9f27a5
roeiad <roeiadlersh@gmail.com>
2020-08-21 12:44:32 +0300

    ss

---
+++ index.js
@@ -0,0 +1,57 @@
+const Discord = require("discord.js");
+const {promisify} = require("util");
+const readdir = promisify(require("fs").readdir);
+const Enmap = require("enmap");
+const client = new Discord.Client();
+client.config = require("./config.js");
+client.logger = require("./modules/Logger");
+require("./modules/functions.js")(client);
+
+client.commands = new Enmap();
+client.aliases = new Enmap();
+
+client.settings = new Enmap({name: "settings"});
+
+
+const init = async () => {
+
+    const cmdFiles = await readdir("./commands/");
+    client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
+    cmdFiles.forEach(f => {
+        if (!f.endsWith(".js")) return;
+        const response = client.loadCommand(f);
+        if (response) console.log(response);
+    });
+
+    const evtFiles = await readdir("./events/");
+    client.logger.log(`Loading a total of ${evtFiles.length} events.`);
+    evtFiles.forEach(file => {
+        const eventName = file.split(".")[0];
+        client.logger.log(`Loading Event: ${eventName}`);
+        const event = require(`./events/${file}`);
+        client.on(eventName, event.bind(null, client));
+    });
+    client.on("ready", () => {
+        console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+        client.user.setActivity(`da!help`);
+    });
+
+    client.on("debug", function (info) {
+        console.log(`debug -> ${info}`);
+    });
+
+    client.on("error", function (error) {
+        console.error(`client's WebSocket encountered a connection error: ${error}`);
+    });
+
+    client.on("disconnect", function (event) {
+        console.log(
+            `The WebSocket has closed and will no longer attempt to reconnect`
+        );
+    });
+
+    client.login(process.env.token);
+
+};
+
+init();
