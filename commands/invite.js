21f1d4e2cb596e31f51ee8ded866c79966392b57
roeiad <roeiadlersh@gmail.com>
2020-08-24 14:42:44 +0300

    ss

--- commands/invite.js
+++ commands/invite.js
@@ -1,16 +1,15 @@
-const text= require("../assets/text.json");
+const text = require("../assets/text.json");
 exports.run = async (client, message) => {
-  let user =message.author.id;
-  await user.send(text.invite);
+    message.author.send(text.invite)
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: "User"
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
 };
 
 exports.help = {
-  name: "invite"
+    name: "invite"
 };
