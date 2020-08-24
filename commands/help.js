2966a3c3a84a5a0b0f3758b28497894f2db48057
roeiad <roeiadlersh@gmail.com>
2020-08-24 17:03:33 +0300

    ss

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,19 @@
 const Discord = require("discord.js");
 const embedes = new Discord.MessageEmbed()
     .setColor(0x00AE86);
+const embcommand = new Discord.MessageEmbed()
+    .setColor(0x00AE86);
 exports.run = (client, message, args) => {
     if (!args[0]) {
         const myCommands = client.commands;
 
-
-        const commandNames = myCommands.keyArray();
-
         let currentCategory = "";
-        let output = ` Command List \n\n[Use ${message.settings.prefix}help <commandname> for details]\n`;
+        let output = ` Command List \n\nUse ${message.settings.prefix}help <commandname> for details\n`;
         const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 : p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1);
         sorted.forEach(c => {
             const cat = c.help.category.toProperCase();
             if (currentCategory !== cat) {
-                embedes.addField('**'+cat +'**',"------------------------");
+                embedes.addField('**' + cat + '**', "**------------------------**");
                 currentCategory = cat;
             }
             embedes.setTitle("help");
@@ -23,14 +22,13 @@ exports.run = (client, message, args) => {
         });
         message.channel.send({embed: embedes});
     } else {
-        // Show individual command's help.
         let command = args[0];
         if (client.commands.has(command)) {
             command = client.commands.get(command);
-            embedes.setTitle(`${command.help.name}`);
-            embedes.setDescription(`${command.help.description}`);
+            embcommand.setTitle(`${command.help.name}`);
+            embcommand.setDescription(`${command.help.description}`);
         }
-        message.channel.send({embed:embedes});
+        message.channel.send({embed: embedes});
     }
 };
 
