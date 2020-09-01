391f7fa7e1b9f21cbe7e3c2afd2269244e428789
roeiad <roeiadlersh@gmail.com>
2020-09-01 11:28:02 +0300

    ss

--- commands/suggest.js
+++ commands/suggest.js
@@ -6,7 +6,7 @@ exports.run = async (client, message) => {
     let description, name;
     let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
 
-    await message.channel.send({embed:{description:"welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
+    await message.channel.send({embed: {description: "welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
 
     await message.channel.send("enter the command name");
 
@@ -28,19 +28,18 @@ exports.run = async (client, message) => {
                 message.channel.send("describe what this command will do");
             } else {
                 description = message.content;
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor("by: " + message.author.tag, message.author.avatarURL())
+                    .setTitle("SUGGESTION:" + name)
+                    .setDescription(description)
+                    .setTimestamp();
 
+                channel.send(embed).then(m => {
+                    m.react("✅");
+                    m.react("❌");
+                });
             }
         }
-        const embed = new Discord.MessageEmbed()
-            .setAuthor("by: " + message.author.tag, message.author.avatarURL())
-            .setTitle("SUGGESTION:" + name)
-            .setDescription(description)
-            .setTimestamp();
-
-        channel.send(embed).then(m => {
-            m.react("✅");
-            m.react("❌");
-        });
     });
 };
 
