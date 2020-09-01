e8b77d32f447bc7720b0e368aa8f8016905b93c7
roeiad <roeiadlersh@gmail.com>
2020-09-01 11:25:01 +0300

    ss

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,32 +1,39 @@
-const text = require("../assets/text.json");
 const Discord = require("discord.js");
 
 
 exports.run = async (client, message) => {
     const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {time: 10000});
-    let description;
-    let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"))
-    await message.channel.send("welcome to the Suggestion center for dabot\n type 'cancel` to cancel");
+    let description, name;
+    let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
 
-    message.channel.send("enter a your suggestion");
+    await message.channel.send({embed:{description:"welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
+
+    await message.channel.send("enter the command name");
 
     collector.on('collect', message => {
         if (message.content === "help") {
             message.channel.send("da!{command name}");
-        }
-        if (message.content === "cancel") {
-            return message.channel.send("cancelling");
-        }
-        if (message.content === "help") {
-            message.channel.send("describe what this command will do");
+        } else if (message.content === "cancel") {
+            return message.channel.send("canceling");
         } else {
-            description = message.content;
+            name = message.content;
+            message.channel.send("enter a description");
+            if (message.content === "help") {
+                message.channel.send("da!{command name}");
+            }
+            if (message.content === "cancel") {
+                return message.channel.send("cancelling");
+            }
+            if (message.content === "help") {
+                message.channel.send("describe what this command will do");
+            } else {
+                description = message.content;
 
+            }
         }
-
         const embed = new Discord.MessageEmbed()
             .setAuthor("by: " + message.author.tag, message.author.avatarURL())
-            .setTitle("SUGGESTION")
+            .setTitle("SUGGESTION:" + name)
             .setDescription(description)
             .setTimestamp();
 
