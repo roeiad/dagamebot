ba127faa7f3ff96de8ad592262d09a12a6e0f2fa
roeiad <roeiadlersh@gmail.com>
2020-09-01 11:39:10 +0300

    ss

--- commands/suggest.js
+++ commands/suggest.js
@@ -2,48 +2,44 @@ const Discord = require("discord.js");
 
 
 exports.run = async (client, message) => {
-    const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {time: 10000});
     let description, name;
     let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
 
     await message.channel.send({embed: {description: "welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
 
     await message.channel.send("enter the command name");
-
-    collector.on('collect', message => {
+    if (message.content === "help") {
+        message.channel.send("da!{command name}");
+    } else if (message.content === "cancel") {
+        return message.channel.send("canceling");
+    } else {
+        name = message.content;
+        message.channel.send("enter a description");
         if (message.content === "help") {
             message.channel.send("da!{command name}");
-        } else if (message.content === "cancel") {
-            return message.channel.send("canceling");
-        } else {
-            name = message.content;
-            message.channel.send("enter a description");
-            if (message.content === "help") {
-                message.channel.send("da!{command name}");
-            }
-            if (message.content === "cancel") {
-                return message.channel.send("cancelling");
-            }
-            if (message.content === "help") {
-                message.channel.send("describe what this command will do");
-            } else {
-                description = message.content;
-
-            }
-            return [name, description];
         }
-        const embed = new Discord.MessageEmbed()
-            .setAuthor("by: " + message.author.tag, message.author.avatarURL())
-            .setTitle("SUGGESTION:" + name)
-            .setDescription(description)
-            .setTimestamp();
+        if (message.content === "cancel") {
+            return message.channel.send("cancelling");
+        }
+        if (message.content === "help") {
+            message.channel.send("describe what this command will do");
+        } else {
+            description = message.content;
+        }
+    }
 
-        channel.send(embed).then(m => {
-            m.react("✅");
-            m.react("❌");
-        });
+    const embed = new Discord.MessageEmbed()
+        .setAuthor("by: " + message.author.tag, message.author.avatarURL())
+        .setTitle("SUGGESTION:" + name)
+        .setDescription(description)
+        .setTimestamp();
+
+    channel.send(embed).then(m => {
+        m.react("✅");
+        m.react("❌");
     });
-};
+}
+;
 
 
 exports.conf = {
