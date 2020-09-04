b879d752fdf440c0bc2c2f307171f6dd532c4750
roeiad <roeiadlersh@gmail.com>
2020-09-04 13:11:30 +0300

    ss

--- commands/suggest.js
+++ commands/suggest.js
@@ -4,16 +4,17 @@ const Discord = require("discord.js");
 exports.run = async (client, message) => {
     let args = [];
     let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
+    const filter = m=>m.author.id === message.author.id;
     const collector = message.channel.createMessageCollector({time: 15000});
 
-    await message.channel.send({embed: {description: "welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
+     message.channel.send({embed: {description: "welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
 
     message.channel.send("what is the name of the command").then(() => {
-        message.channel.awaitMessages({max: 1, time: 30000, errors: ['time']})
+        message.channel.awaitMessages(filter,{max: 1, time: 30000, errors: ['time']})
             .then(collected => {
                 args.push(collected.first());
                 message.channel.send(`describe the command`).then(() => {
-                    message.channel.awaitMessages({max: 1, time: 30000, errors: ['time']})
+                    message.channel.awaitMessages(filter,{max: 1, time: 30000, errors: ['time']})
                         .then(collected => {
                             args.push(collected.first());
                             const embed = new Discord.MessageEmbed()
