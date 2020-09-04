af525d598da624970696c3401c4dc12a7270f527
roeiad <roeiadlersh@gmail.com>
2020-09-04 13:01:43 +0300

    ss

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,50 +3,39 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message) => {
     let args = [];
-    // let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
+    let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
     const collector = message.channel.createMessageCollector({time: 15000});
 
     await message.channel.send({embed: {description: "welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
 
-    await message.channel.send("enter the command name");
+    message.channel.send("what is the name of the command").then(() => {
+        message.channel.awaitMessages({max: 1, time: 30000, errors: ['time']})
+            .then(collected => {
+                args.push(collected.first());
+                message.channel.send(`describe the command`).then(() => {
+                    message.channel.awaitMessages({max: 1, time: 30000, errors: ['time']})
+                        .then(collected => {
+                            args.push(collected.first());
+                            const embed = new Discord.MessageEmbed()
+                                .setAuthor("by: " + message.author.tag, message.author.avatarURL())
+                                .setTitle("SUGGESTION:" + args[0])
+                                .setDescription(args[1])
+                                .setTimestamp();
 
-    collector.on('collect', message => {
-        if (message.content === "help") {
-            message.channel.send("da!{command name}");
-        } else if (message.content === "cancel") {
-            return message.channel.send("canceling");
-        } else {
-            args.push(message.content);
-        }
-    });
-    collector.on('collect', message => {
-        message.channel.send("enter a description");
-        if (message.content === "help") {
-            message.channel.send("da!{command name}");
-        }
-        if (message.content === "cancel") {
-            return message.channel.send("cancelling");
-        }
-        if (message.content === "help") {
-            message.channel.send("describe what this command will do");
-        } else {
-            args.push(message.content);
-        }
-    });
+                            channel.send(embed).then(m => {
+                                m.react("✅");
+                                m.react("❌");
+                            });
+                        });
+                })
 
-        // const embed = new Discord.MessageEmbed()
-        //     .setAuthor("by: " + message.author.tag, message.author.avatarURL())
-        //     .setTitle("SUGGESTION:" + args[0])
-        //     .setDescription(args[1])
-        //     .setTimestamp();
-        //
-        // await channel.send(embed).then(m => {
-        //     m.react("✅");
-        //     m.react("❌");
-        // });
-        // args = [];
+                    .catch(collected => {
+                        message.channel.send('canceling');
+                    });
+            });
+
+    });
 };
-
 exports.conf = {
     enabled: true,
     guildOnly: false,
