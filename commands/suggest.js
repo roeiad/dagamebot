ba28db95eb2129abeb511bd2d1a52b5ceed7d5c6
roeiad <roeiadlersh@gmail.com>
2020-09-04 13:55:21 +0300

    ss

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,22 +1,18 @@
 const Discord = require("discord.js");
-
-
 exports.run = async (client, message) => {
     let args = [];
     let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
-    const filter = m=> {
+    const filter = m => {
         return m.author.id === message.author.id;
-};
-    // const collector = message.channel.createMessageCollector({time: 15000});
+    };
 
-     message.channel.send({embed: {description: "welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
-
+    message.channel.send({embed: {description: "welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
     message.channel.send("what is the name of the command").then(() => {
-        message.channel.awaitMessages(filter,{max: 1, time: 30000, errors: ['time']})
+        message.channel.awaitMessages(filter, {max: 1, time: 10000, errors: ['time']})
             .then(collected => {
                 args.push(collected.first().content);
                 message.channel.send(`describe the command`).then(() => {
-                    message.channel.awaitMessages(filter,{max: 1, time: 30000, errors: ['time']})
+                    message.channel.awaitMessages(filter, {max: 1, time: 10000, errors: ['time']})
                         .then(collected => {
                             args.push(collected.first().content);
                             const embed = new Discord.MessageEmbed()
@@ -28,15 +24,20 @@ exports.run = async (client, message) => {
                             channel.send(embed).then(m => {
                                 m.react("✅");
                                 m.react("❌");
+                                args = [];
                             });
+
+
+                        })
+                        .catch(collected => {
+                            message.channel.send('canceling');
                         });
-                })
+                });
 
-                    .catch(collected => {
-                        message.channel.send('canceling');
-                    });
+            })
+            .catch(collected => {
+                message.channel.send('canceling');
             });
-
     });
 };
 exports.conf = {
