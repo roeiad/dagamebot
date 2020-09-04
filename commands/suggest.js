22caa6ff57a8ce46d9352f17f1160f8285d38f4e
roeiad <roeiadlersh@gmail.com>
2020-09-04 13:36:11 +0300

    ss

--- commands/suggest.js
+++ commands/suggest.js
@@ -4,8 +4,10 @@ const Discord = require("discord.js");
 exports.run = async (client, message) => {
     let args = [];
     let channel = message.guild.channels.cache.find((x) => (x.name === "suggestion" || x.name === "suggestions"));
-    const filter = m=>m.author.id === message.author.id;
-    const collector = message.channel.createMessageCollector({time: 15000});
+    const filter = m=> {
+        return m.author.id === message.author.id;
+};
+    // const collector = message.channel.createMessageCollector({time: 15000});
 
      message.channel.send({embed: {description: "welcome to the Suggestion center for dabot\n if in any part you having trouble, type 'help'\n 'cancel` to cancel"}});
 
