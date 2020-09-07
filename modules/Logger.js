da25d4136fa3f6effa7e27c853ca91ba4576c6a4
roeiad <roeiadlersh@gmail.com>
2020-09-07 17:59:14 +0300

    ss

--- modules/Logger.js
+++ modules/Logger.js
@@ -5,29 +5,30 @@ const chalk = require("chalk");
 const moment = require("moment");
 
 exports.log = (content, type = "log") => {
-  const timestamp = `[${moment().format("DD-MM-YYY HH:mm:ss")}]:`;
-  switch (type) {
-    case "log": {
-      return console.log(`${timestamp} ${chalk.bgBlue(type[0].toUpperCase())} ${content} `);
+    const timestamp = `[${moment().format("DD-MM-YYY HH:mm:ss")}]:`;
+    switch (type) {
+        case "log": {
+            return console.log(`${timestamp} ${chalk.bgBlue(type[0].toUpperCase() + type.slice(1))} ${content} `);
+        }
+        case "warn": {
+            return console.log(`${timestamp} ${chalk.black.bgYellow(type[0].toUpperCase() + type.slice(1))} ${content} `);
+        }
+        case "error": {
+            return console.log(`${timestamp} ${chalk.bgRed(type[0].toUpperCase() + type.slice(1))} ${content} `);
+        }
+        case "debug": {
+            return console.log(`${timestamp} ${chalk.green(type[0].toUpperCase() + type.slice(1))} ${content} `);
+        }
+        case "cmd": {
+            return console.log(`${timestamp} ${chalk.black.bgWhite(type[0].toUpperCase() + type.slice(1))} ${content}`);
+        }
+        case "ready": {
+            return console.log(`${timestamp} ${chalk.black.bgGreen(type[0].toUpperCase() + type.slice(1))} ${content}`);
+        }
+        default:
+            throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
     }
-    case "warn": {
-      return console.log(`${timestamp} ${chalk.black.bgYellow(type[0].toUpperCase())} ${content} `);
-    }
-    case "error": {
-      return console.log(`${timestamp} ${chalk.bgRed(type[0].toUpperCase())} ${content} `);
-    }
-    case "debug": {
-      return console.log(`${timestamp} ${chalk.green(type[0].toUpperCase())} ${content} `);
-    }
-    case "cmd": {
-      return console.log(`${timestamp} ${chalk.black.bgWhite(type[0].toUpperCase())} ${content}`);
-    }
-    case "ready": {
-      return console.log(`${timestamp} ${chalk.black.bgGreen(type[0].toUpperCase())} ${content}`);
-    }
-    default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
-  }
-}; 
+};
 
 exports.error = (...args) => this.log(...args, "error");
 
