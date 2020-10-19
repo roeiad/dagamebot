a00c45426c46eb74188228477ba91f1c6a668eda
roeiad <roeiadlersh@gmail.com>
2020-10-19 17:09:27 +0300

    d

--- commands/pet.js
+++ commands/pet.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const text= require("../assets/text.json");
 const pics = require("../assets/pics.json");
 const Discord = require("discord.js");
@@ -28,34 +27,3 @@ exports.help = {
     SDescription: text.help.pet,
     LDescription: text.help.pet
 };
-=======
-const text= require("../assets/text.json");
-const pics = require("../assets/pics.json");
-const Discord = require("discord.js");
-
-
-exports.run = async (client, message) => {
-        const embed = new Discord.MessageEmbed()
-            .setTitle(text.pet)
-            .setColor(0x00AE86)
-            .setImage(pics.plush);
-
-        return await message.channel.send({embed});
-    
-};
-
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [""],
-    permLevel: "User"
-};
-
-exports.help = {
-    name: "pet",
-    usage:"pet",
-    category: "fun",
-    SDescription: "pet the little boi",
-    LDescription: "pet the little boi"
-};
->>>>>>> 31520233941eacafcab5bc8f91efbacc744fe2b9
