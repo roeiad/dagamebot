481ece02598ad55ebd1f37a419fd1fce8fe45352
roeiad <roeiadlersh@gmail.com>
2020-10-20 10:04:55 +0300

    uptade music command

--- commands/music.js
+++ commands/music.js
@@ -2,14 +2,26 @@ const text = require("../assets/text.json");
 const links = require("../assets/links.json");
 exports.run = async (client, message) => {
     // let name="da!music";
-    const arg = message.content.split(' ');
+    let choose =message.arg[0]
+    switch (choose) {
+        default:
+            await message.channel.send(links.playlist);
+            await message.channel.send(links.iris);
+            break;
+        case "iris":
+            await message.channel.send(links.iris);
+            break;
+        case "dagames":
+            await message.channel.send(links.playlist);
+            break;      
+    }
 
-    if (arg[1] === "iris") {
-        await message.channel.send(links.iris);
-    }
-    else {
-        await message.channel.send(links.playlist);
-    }
+    // if (arg[1] === "iris") {
+    //     await message.channel.send(links.iris);
+    // }
+    // else {
+    //     await message.channel.send(links.playlist);
+    // }
 };
 
 exports.conf = {
